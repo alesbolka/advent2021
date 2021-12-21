3835db66faac07ea9d15133cebbe190b99775563
Ales Bolka <ales.bolka@paradoxinteractive.com>
2021-12-21 15:56:11 +0100

    added more code to cpp sample

--- src/shared/sample.cpp
+++ src/shared/sample.cpp
@@ -1,3 +1,17 @@
 #include "./dayXX.h"
+#include "../shared/fileReader.h"
 
 using namespace dayXX;
+
+int dayXX::executeTask(int task)
+{
+  std::vector<std::string> input = readArrayString("./src/dayXX/dayXX.input");
+  std::vector<std::string> sample = readArrayString("./src/dayXX/dayXX.sample");
+
+  if (task == 1)
+  {
+    return 0;
+  }
+
+  return 1;
+}
